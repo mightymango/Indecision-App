9d9da8b190c9e7a0436fc162eda1530b91937628
Simon Barrett <simon@mightymango.ltd>
2019-05-31 12:45:42 +0100

    Split into files

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public', 'scripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+
+    }
+}
