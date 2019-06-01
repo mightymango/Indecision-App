ce3d44855370c532226f4a181de780c8fe93b0d9
Simon Barrett <simon@mightymango.ltd>
2019-06-01 10:59:19 +0100

    Final App

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
         contentLabel="Selected Option"
         appElement={document.getElementById('app')}
         onRequestClose={props.handleClearSelection}
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelection}>OK</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelection}>OK</button>
     </Modal>
 );
 
